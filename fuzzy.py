6ff38647007df54b9d00f76980360dfc5723dc0a
AdrianIchiro <adrianfahren10@gmail.com>
2025-05-04 13:59:56 +0700

    membuat rumus segitiga

---
+++ fuzzy.py
@@ -0,0 +1,13 @@
+import pandas as pd
+
+# fungsi segitiga
+
+def triangular(x, a, b, c):
+    if x <= a or x >= c:
+        return 0.0
+    elif x == b:
+        return 1.0
+    elif x < b:
+        return (x - a) / (b - a)
+    else:
+        return (c - x) / (c - b)

65a80f61d157ca87d04c908dea7e9c4f72c6ae50
KoifishVEVO <149301244+KoifishVEVO@users.noreply.github.com>
2025-05-04 14:28:37 +0700

    mambuat inferense dan defuzzifksai

--- fuzzy.py
+++ fuzzy.py
@@ -26,3 +26,37 @@ def fuzzify_harga(value):
         'mahal': triangular(value, 45000, 50000, 55000)
     }
 
+
+def inferensi(servis, harga):
+    rules = []
+
+    rules.append(('rendah', 'mahal', 'tidak_layak'))
+    rules.append(('tinggi', 'murah', 'sangat_layak'))
+    rules.append(('sedang', 'sedang', 'layak'))
+    rules.append(('tinggi', 'sedang', 'layak'))
+    rules.append(('sedang', 'murah', 'layak'))
+    rules.append(('rendah', 'murah', 'layak'))
+    rules.append(('tinggi', 'mahal', 'layak'))
+    rules.append(('rendah', 'sedang', 'tidak_layak'))
+    rules.append(('sedang', 'mahal', 'tidak_layak'))
+
+    hasil = {'tidak_layak': 0.0, 'layak': 0.0, 'sangat_layak': 0.0}
+
+    for rule in rules:
+        s, h, o = rule
+        nilai = min(servis[s], harga[h])
+        hasil[o] = max(hasil[o], nilai)
+
+    return hasil
+
+
+def defuzzifikasi(output):
+    bobot = {
+        'tidak_layak': 25,
+        'layak': 60,
+        'sangat_layak': 90
+    }
+    atas = sum(output[label] * bobot[label] for label in output)
+    bawah = sum(output[label] for label in output)
+    return atas / bawah if bawah != 0 else 0
+
